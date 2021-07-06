b72c09402018521016012d495bec2ae7f14b324d
nicochabmelo@gmail.com <82840508+NicoChabot@users.noreply.github.com>
2021-07-06 20:10:42 -0300

    commit

--- Codigo/js/database.js
+++ Codigo/js/database.js
@@ -106,33 +106,25 @@ $(function() {
     tbUsuarios = JSON.parse(tbUsuarios);
     if (tbUsuarios = null)
         tbUsuarios = [];
-    else
-        validate();
 })
 
 function Add() {
     var user = JSON.stringify({
-        email: $("#email").val(),
-        usuario: $("#name").val(),
-        senha: $("#password").val(),
-        tipo: $("#tipo").val(),
-        frequencia: $("#Freq").val(),
-        idade: $("#Id").val()
+        email: $("#txtemail").val(),
+        usuario: $("#txtname").val(),
+        senha: $("#txtpassword").val(),
+        tipo: $("#txttipo").val(),
+        frequencia: $("#txtFreq").val(),
+        idade: $("#txtId").val()
     });
     tbUsuarios.push(user);
     localStorage.setItem("tbUsuarios", JSON.stringify(tbUsuarios));
     alert("Registro adicionado.");
     operação = "A";
     return true;
-
 }
-$("#cadastro").on("submite", function() {
-    if (operação == "A")
-        return Add();
-    else
-        return alert("Cadastro já efetuado, faça login.");
-})
 
+<<<<<<< Updated upstream
 function validate() {
     user.usuario = document.getElementById(usuario);
     if (val() == 0)
@@ -144,9 +136,29 @@ function validate() {
     else
         return "#btnEntrar";
 }
+=======
+function editUser() {
+    tbUsuarios[indice_selecionado] = JSON.stringify({
 
-function show() {
 
+            }
+
+            function validate() {
+                var user = document.getElementById(usuario);
+                if (val() == 0)
+                    alert(Usuario_inexistente);
+                else
+                    var user = document.getElementById(senha);
+                if (val() == 0)
+                    alert(Senha_incorreta)
+                else
+                    return "#btnEntrar";
+            }
+>>>>>>> Stashed changes
+
+            function show() {
+
+<<<<<<< Updated upstream
     let data = JSON.parse(xhr.responseText);
     let textoHTML = '';
     if ($("#name").val() != 0) {
@@ -158,3 +170,20 @@ function show() {
 }
 
 */
+=======
+                let data = JSON.parse(xhr.responseText);
+                let textoHTML = '';
+                if ($("#name").val() != 0) {
+                    textoHTML += '<div class="user"><p class = "user-name" >Olá, ${usuario} < /p> <i class = "fas fa-user-circle" > < /i> < /div > '
+                } else {
+                    textoHTML += '<div class="user"> <p class = "user-name" > Olá, user< /p> <i class = "fas fa-user-circle" > < /i> < /div > '
+                }
+                document.getElementById('tela').innerHTML = textoHTML;
+            }
+            $("#cadastro").on("submite", function() {
+                if (operação == "A")
+                    return Add();
+                else
+                    return alert("Cadastro já efetuado, faça login.");
+            })
+>>>>>>> Stashed changes
