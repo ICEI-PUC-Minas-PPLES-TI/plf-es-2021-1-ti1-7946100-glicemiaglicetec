c386770a8adf075b79084a7a1a9d9f1bf812dfe3
nicochabmelo@gmail.com <82840508+NicoChabot@users.noreply.github.com>
2021-07-06 23:52:03 -0300

    commit

--- Codigo/js/database.js
+++ Codigo/js/database.js
@@ -1,88 +1,99 @@
-$(function() {
-    var operação = "A";
-    var indice_selecionado = -1;
-    var tbUsuarios = localStorage.getItem("tbUsuarios");
-    tbUsuarios = JSON.parse(tbUsuarios);
-    if (tbUsuarios = null)
-        tbUsuarios = [];
-});
+// declara um conjunto inicial de contatos
+var db_diabeticos_inicial = {
+    "data": [{
+            "id": 1,
+            "nome": "Venancia Matias",
+            "Tipo_Diabetes": "Tipo 3",
+            "email": "veve@gmail.com",
+            "data_nasciment": "17/04/1942",
+            "senha": "Craqueneto1704"
 
-function Add() {
-    var user = JSON.stringify({
-        email: $("#txtemail").val(),
-        usuario: $("#txtname").val(),
-        senha: $("#txtpassword").val(),
-        tipo: $("#txttipo").val(),
-        frequencia: $("#txtFreq").val(),
-        idade: $("#txtId").val()
-    });
-    tbUsuarios.push(user);
-    localStorage.setItem("tbUsuarios", JSON.stringify(tbUsuarios));
-    alert("Registro adicionado.");
-    operação = "A";
-    return true;
+        },
+        {
+            "id": 2,
+            "nome": "Peter Parquer",
+            "Tipo_Diabetes": "Tipo 1",
+            "email": "miranha@gmail.com",
+            "data_nasciment": "22/02/1998",
+            "senha": "vaiteia22"
+        },
+        {
+            "id": 3,
+            "nome": "Tony Esterco",
+            "Tipo_Diabetes": "Pré-diabético",
+            "email": "tonyesterco@gmail.com",
+            "data_nasciment": "22/08/02",
+            "senha": "Iamtheironman"
+        },
+        {
+            "id": 4,
+            "nome": "Thor Sonofodin",
+            "Tipo_Diabetes": "Tipo 2",
+            "email": "melhorvingador@gmail.com",
+            "data_nasciment": "04/04/2004",
+            "senha": "Filhinhodepapai"
+
+        },
+        {
+            "id": 5,
+            "nome": "Estevão Rogério",
+            "Tipo_Diabetes": "Tipo 4",
+            "email": "picole@gmail.com",
+            "data_nasciment": "13/05/1972",
+            "senha": "picoleidoso"
+
+        },
+        {
+            "id": 6,
+            "nome": "Doutor Normal",
+            "Tipo_Diabetes": "Tipo 5",
+            "email": "normal@outlook.com",
+            "data_nasciment": "14/07/1983",
+            "senha": "magodasmagias1"
+
+        },
+    ]
+}
+var db = JSON.parse(localStorage.getItem('db_diabeticos'));
+if (!db) {
+    db = db_diabeticos_inicial
+};
+
+function displayMessage(msg) {
+    $('#msg').html('<div class="alert alert-warning">' + msg + '</div>');
 }
 
-function editUser() {
-    tbUsuarios[indice_selecionado] = JSON.stringify({
-        usuario: $("txtname").val(),
-        email: $("#txtemail").val(),
-        usuario: $("#txtname").val(),
-        senha: $("#txtpassword").val(),
-        tipo: $("#txttipo").val(),
-        frequencia: $("#txtFreq").val(),
-    });
-    localStorage.setItem("tbUsuarios", JSON.stringify(tbUsuarios));
-    alert("Informações editadas.")
-    operacao = "A";
-    return true;
+function insertDiabetico(diabetico) {
+    let novoId = db.data[db.data.length - 1].id + 1;
+    let novoDiabetico = {
+        "id": novoId,
+        "nome": diabetico.nome,
+        "Tipo_Diabetes": diabetico.Tipo_Diabetes,
+        "email": diabetico.email,
+        "data_nasciment": diabetico.data_nasciment,
+        "senha": diabetico.senha,
+    };
+
+    db.data.push(novoDiabetico);
+    displayMessage("Cadastro inserido com sucesso");
+    localStorage.setItem('db_contato', JSON.stringify(db));
 }
 
-function login(email, senha) {
-    if (typeof email == 'string' && typeof password == 'string' && email.length > 0 && senha.length > 0) {
-        var loggeduser;
-        var user = users[index];
-        if (email === user.email && senha === user.senha)
-            loggeduser = user;
-    }
-    if (typeof loggeduser != 'undefined') {
-        loggedusers[loggeduser.id] = true;
-        updatelist();
-        return loggeduser;
-    }
+function updateDiabetico(id, diabetico) {
+    let index = db.data.map(obj => obj.id).indexOf(id);
+    db.data[index].nome = diabetico.nome,
+        db.data[index].email = diabetico.email,
+        db.data[index].Tipo_Diabetes = diabetico.Tipo_Diabetes,
+        db.data[index].data_nasciment = diabetico.data_nasciment,
+        db.data[index].senha = diabetico.senha,
+
+        displayMessage("Cadastro alterado com sucesso");
+    localStorage.setItem('db_contato', JSON.stringify(db));
 }
 
-return false;
+function deleteDiabetico(id) {
+    db.data = db.data.filter(function(element) { return element.id != id });
 
-function logout(usuario) {
-    if (loggedusers[usuario]) {
-        var temporary = [];
-        for (var id in loggedusers)
-            if (id != usuario)
-                temporary[id] = true;
-        loggedusers = temporary;
-        updatelist();
-        return true;
-    }
-    return false;
-}
-
-function show() {
-
-    let data = JSON.parse(xhr.responseText);
-    let textoHTML = '';
-    if ($("#name").val() != 0) {
-        textoHTML += '<div class="user"><p class = "user-name" >Olá, ${usuario} < /p> <i class = "fas fa-user-circle" > < /i> < /div > '
-    } else {
-        textoHTML += '<div class="user"> <p class = "user-name" > Olá, user< /p> <i class = "fas fa-user-circle" > < /i> < /div > '
-    }
-    document.getElementById('tela').innerHTML = textoHTML;
-    let data = JSON.parse(xhr.responseText);
-    let textoHTML = '';
-    if ($("#name").val() != 0) {
-        textoHTML += '<div class="user"><p class = "user-name" >Olá, ${usuario} < /p> <i class = "fas fa-user-circle" > < /i> < /div > '
-    } else {
-        textoHTML += '<div class="user"> <p class = "user-name" > Olá, user< /p> <i class = "fas fa-user-circle" > < /i> < /div > '
-    }
-    document.getElementById('tela').innerHTML = textoHTML;
+    displayMessage("Cadastro removido com sucesso");
+    localStorage.setItem('db_diabetico', JSON.stringify(db));
 }
